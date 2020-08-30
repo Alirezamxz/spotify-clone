7a0b1ae021eadcca99154040753cac1066ca499c
Alirezamxz <alirezamz.org@Gmail.com>
2020-08-30 14:34:56 +0430

    add state for login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Login from './components/Login/Login'
+import Player from './components/Player/Player'
+import { getTokenFromUrl } from './spotify'
 
 function App() {
+
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+    window.location.hash = ""
+    const _token = hash.access_token
+    if (_token) {
+      setToken(_token)
+    }
+  } ,[])
+
   return (
     <div className="app">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        token ? ( <Player /> ) : ( <Login /> )
+      }
     </div>
   );
 }
 
 export default App;
+
+// https://youtu.be/pnkuI8KXW_8?t=4819
