e82990ee05cfba3354e5587cd4b83b68e22c2ac4
Alirezamxz <alirezamz.org@Gmail.com>
2020-09-04 12:42:02 +0430

    player component

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Player.css'
+import Sidebar from '../SideBar/Sidebar'
+import Body from '../Body/Body'
+import Footer from '../Footer/Footer'
 
 function Player() {
     return (
-        <div>
-            <h2>welcome to spotify</h2>
+        <div className="player">
+            <div className="player__body">
+                <Sidebar />
+                <Body />
+            </div>
+
+            <Footer />
+
         </div>
     )
 }
