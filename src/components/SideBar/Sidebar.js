1755ed3c93f7812458b7a14459d3a1be34341d5e
Alirezamxz <alirezamz.org@Gmail.com>
2020-09-04 13:21:00 +0430

    sidebar data and style

--- src/components/SideBar/Sidebar.js
+++ src/components/SideBar/Sidebar.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import './Sidebar.css'
 import SidebarOption from '../SidebarOption/SidebarOption'
+import HomeIcon from '@material-ui/icons/Home'
+import SearchIcon from '@material-ui/icons/Search'
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic'
 
 function SideBar() {
     return (
         <div className="sidebar">
             <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"alt="spotify-logo"/>
-            <SidebarOption title="Home" />
-            <SidebarOption title="Search" />
-            <SidebarOption title="Your Library" />
+            <SidebarOption title="Home" Icon={HomeIcon} />
+            <SidebarOption title="Search" Icon={SearchIcon} />
+            <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
+            <strong className="sidebar__title">Playlists</strong>
+            <hr />
+            <SidebarOption title="Rock" />
+            <SidebarOption title="Heavy Metal" />
+            <SidebarOption title="Chill" />
         </div>
     )
 }
