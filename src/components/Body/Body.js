56ecfa151229afc83ecab3d515c8ffdb9c271ca8
Alirezamxz <alirezamz.org@Gmail.com>
2020-09-06 23:36:52 +0430

    add header to body of player

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Body.css'
+import Header from '../Header/Header'
 
-function Body() {
+function Body({ spotify }) {
     return (
         <div className="body">
-            <h1>im a body</h1>
+            <Header spotify={spotify}/>
+
+            <div className="body__info">
+                <img src="https://newjams-images.scdn.co/v2/discover-weekly/pOdnPTRma6KZzKPNRN9oUA==/bmVuZW5lbmVuZW5lbmVuZQ==/default" />
+                <div className="body__infoText">
+                    <strong>Playlist</strong>
+                    <h2>Discover Weekly</h2>
+                    <p>description...</p>
+                </div>
+            </div>
         </div>
     )
 }
