1616ff03173d7482bca07df317fc3086159e72b0
Alirezamxz <alirezamz.org@Gmail.com>
2020-09-07 00:32:24 +0430

    remove token

--- src/states/Reducer.js
+++ src/states/Reducer.js
@@ -3,7 +3,6 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    // token: "BQBL6gb-G5AefgAkAzgPWAs4-phA3A42zJPY38RsyeoM-tzx9IGS468nodlkx-oJ8RaNvIrh134naUgayXZdGjPGhHTM1E8ZIb2wi2KyxpHJDq9fHwKUnqZN_KOzmnLLgDOU-mmf6DP4UgmXfwUC-xX0ui2ajJ65kjTT2Wg-u2AVFBYMdyu7",
 }
 
 const reducer = (state, action) => {
@@ -22,6 +21,11 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 playlists: action.playlists,
+            }
+        case 'SET_DISCOVER_WEEKLY':
+            return {
+                ...state,
+                discover_weekly: action.discover_weekly
             }
         default:
             return state
