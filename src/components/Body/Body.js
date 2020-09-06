357523319af87f9a917a1dad5b165d8a78796366
Alirezamxz <alirezamz.org@Gmail.com>
2020-09-07 00:32:51 +0430

    body component

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,19 +1,38 @@
 import React from 'react'
 import './Body.css'
 import Header from '../Header/Header'
+import { useDataLayerValue } from '../../states/DataLayer'
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import MoreHorizIocn from '@material-ui/icons/MoreHoriz'
+import SongRow from '../SongRow/SongRow'
 
 function Body({ spotify }) {
+
+    const [{ discover_weekly }, dispatch] = useDataLayerValue()
+
     return (
         <div className="body">
             <Header spotify={spotify}/>
 
             <div className="body__info">
-                <img src="https://newjams-images.scdn.co/v2/discover-weekly/pOdnPTRma6KZzKPNRN9oUA==/bmVuZW5lbmVuZW5lbmVuZQ==/default" />
+                <img src={discover_weekly?.images[0]?.url} />
                 <div className="body__infoText">
                     <strong>Playlist</strong>
                     <h2>Discover Weekly</h2>
-                    <p>description...</p>
+                    <p>{discover_weekly?.description}</p>
                 </div>
+            </div>
+
+            <div className="body__songs">
+                <div className="body__icons">
+                    <PlayCircleFilledIcon className="body__shuffle"/>
+                    <FavoriteIcon fontSize="large"/>
+                    <MoreHorizIocn />
+                </div>
+                {discover_weekly?.tracks.items.map(item => (
+                    <SongRow track={item.track} />
+                ))}
             </div>
         </div>
     )
